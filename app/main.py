568c9a1bb274579d839949801b96e0888cebfcec
Margo <slava1kvartovkin@gmail.com>
2021-09-24 10:38:35 +0300

    fix: right commons

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
-from fastapi import FastAPI
+from app.db.models import RouteType
+from fastapi import FastAPI, Depends
 from typing import List
 
 from app.db.schemas import Route
 
 app = FastAPI(title='active-petersburg', version='1.0.0')
 
+
+async def common_parameters(distance: float, tags: str, type: RouteType = RouteType.running.value):
+    return {"type": type, "distance": distance, "tags": tags}
+
 @app.get('/routes', response_model=List[Route])
-def get_routes(str: str): 
+async def get_routes(commons: dict = Depends(common_parameters)):
     one_route = {
     'type': 'wheel', 
     'distance': 3.9, 
     'fact': 'lol',
     'url': ''}
-    routes = [one_route]
+    routes = [one_route, one_route, one_route, one_route]
     return routes
