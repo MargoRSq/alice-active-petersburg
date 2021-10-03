4a374db5bdf54e5ad31e9f0ab84095d135432dc5
Margo <slava1kvartovkin@gmail.com>
2021-10-03 21:11:25 +0300

    fix: heroku port in env

--- app/utils/config.py
+++ app/utils/config.py
@@ -3,8 +3,8 @@ from starlette.config import Config
 config = Config(".env")
 
 DATABASE_URL: str = config("SQLALCH_DATABASE_URL")
-
 YW_TOKEN: str = config("YW_TOKEN")
+HOST: str = config("HOST")
 
 # GS_CLIENT_ID: str = config("GS_CLIENT_ID")
 # GS_CLIENT_SECRET: str = config("GS_CLIENT_SECRET")
