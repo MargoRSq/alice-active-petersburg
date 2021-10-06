a4fbc491bcfa05ed4b9678e33cdbc9961ed9686b
Margo <slava1kvartovkin@gmail.com>
2021-10-06 15:40:54 +0300

    mend

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import uvicorn
 from sys import argv
 from app.utils.config import HOST
 
-if __name__ == '__main__':`
+if __name__ == '__main__':
     uvicorn.run("app.app:app", host=HOST, port=int(argv[1]), reload=True)
