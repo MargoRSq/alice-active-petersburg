ecba38077f0fadd3ed80619960c26ee6bd4180b5
Margo <slava1kvartovkin@gmail.com>
2021-10-29 08:46:30 +0300

    weather start

--- app/utils/config.py
+++ app/utils/config.py
@@ -3,4 +3,3 @@ from starlette.config import Config
 config = Config(".env")
 
 DATABASE_URL: str = config("SQLALCH_DATABASE_URL")
-YW_TOKEN: str = config("YW_TOKEN")
--- app/utils/weather.py
+++ app/utils/weather.py
@@ -1,10 +1,7 @@
 import requests
 
-from app.utils.config import YW_TOKEN
 
-
-headers = {'X-Yandex-API-Key': YW_TOKEN}
-url = 'https://api.weather.yandex.ru/v2/informers/?lat=59.9311&lon=30.3609&lang=ru&limit=1'
+url = 'https://wttr.in/stpetersburg?format=j1'
 
 windy_cond = 'На улице сейчас сильный ветерок, дважды подумайте о том, что вы наденете!' #ветренно
 cold_cond = 'В Петербурге сейчас холодновато, одевайтесь потеплее!' #холодно
@@ -14,11 +11,12 @@ perfect_cond = 'На улице сейчас идеальная погода д�
 
 
 def get_weather():
-    r = requests.get(url, headers=headers)
-    print(r.status_code)
-    json_fact = r.json()['fact']
+    r = requests.get(url)
+    from pprint import pprint
+    pprint(r.json())
+    json_fact = r.json()['current_condition']
     condition = json_fact['condition']
-    feels_like = json_fact['feels_like']
+    feels_like = json_fact['FeelsLikeC']
     wind_speed = json_fact['wind_speed']
 
     if 'rain' in condition:
